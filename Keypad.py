478b82af561647595b74e2f6905858d34e46ba19
EricPy <87114549+EricPy@users.noreply.github.com>
2022-04-10 23:05:11 +0700

    Update Keypad.py

--- Keypad.py
+++ Keypad.py
@@ -8,40 +8,23 @@ user_input.grid(row=0,columnspan=4,padx=5,pady=5)
 
 
 def insertion(n):
-    user_input.insert(0,str(n))
+    user_input.insert(-1,str(n))
 
 Button(root,padx=12,pady=12,text="C").grid(column=0,row=1)
 Button(root,padx=85,pady=12,text="=").grid(column=1,row=1,columnspan=3)
 
-col = 2
-ro = 5
-num = 9
-nums = list(range(0,10))
 #Number pad
-'''
-Need to find a way to make it so each number button is assigned its own unique value
-'''
+button7 = Button(root, padx=12, pady=12, text=str(7),command=lambda: insertion(7)).grid(column=0,row=2)
+button8 = Button(root, padx=12, pady=12, text=str(8),command=lambda: insertion(8)).grid(column=1,row=2)
+button9 = Button(root, padx=12, pady=12, text=str(9),command=lambda: insertion(9)).grid(column=2,row=2)
 
-for i in nums:
-    #pdb.set_trace()
-    if i == 0:
-        col = 1
-        Button(root, padx=12, pady=12, text=str(0),command=lambda: insertion(0)).grid(column=col,row=ro)
-        ro-=1
-        col=0
-    else:
-        Button(root, padx=12, pady=12, text=str(nums[i]),command=lambda: insertion(nums.pop())).grid(column=col,row=ro)
-        col += 1
-        if col == 3:
-            col = 0
-            ro-=1
+button4 = Button(root, padx=12, pady=12, text=str(4),command=lambda: insertion(4)).grid(column=0,row=3)
+button5 = Button(root, padx=12, pady=12, text=str(5),command=lambda: insertion(5)).grid(column=1,row=3)
+button6 = Button(root, padx=12, pady=12, text=str(6),command=lambda: insertion(6)).grid(column=2,row=3)
 
-#Operations
-ro2=2
-
-for sign in ["+","-","÷","x"]:
-    Button(root, padx=12, pady=12, text=sign).grid(column=3,row=ro2)
-    ro2+=1
+button1 = Button(root, padx=12, pady=12, text=str(1),command=lambda: insertion(1)).grid(column=0,row=4)
+button2 = Button(root, padx=12, pady=12, text=str(2),command=lambda: insertion(2)).grid(column=1,row=4)
+button3 = Button(root, padx=12, pady=12, text=str(3),command=lambda: insertion(3)).grid(column=2,row=4)
 
 
 root.mainloop()
